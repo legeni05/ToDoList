c602a559304e5d7109483e984b79094ae42affdf
inno5vivi@gamil.com <[inno5vivi@gmail.com]>
2024-12-01 13:45:23 +0000

    Mon App ToFoList

---
+++ models.py
@@ -0,0 +1,32 @@
+# models.py
+from werkzeug.security import generate_password_hash, check_password_hash
+from db import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    def is_active(self):
+        return True
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    date = db.Column(db.String(10), nullable=False)
+    time = db.Column(db.String(5), nullable=False)
+
+
+
